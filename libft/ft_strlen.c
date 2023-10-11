4dfa3d13d793f253ae222a6be1d5fa92731c5c74
Belen Herranz Campusano <bherranz@c2r12s2.42madrid.com>
2023-10-11 17:49:09 +0200

    Created github repo

---
+++ libft/ft_strlen.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bherranz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 13:42:48 by bherranz          #+#    #+#             */
+/*   Updated: 2023/09/16 13:42:54 by bherranz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strlen(const char *str)
+{
+	int	count;
+
+	count = 0;
+	while (str[count])
+	{
+		count++;
+	}
+	return (count);
+}
+
+/*int	main(void)
+{
+	printf("%i", ft_strlen("jaime"));
+	return (0);
+}*/
