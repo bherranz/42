a07b84bd81996aff5908aa8139c7ce3443dd03ed
bherranz <91571076+bherranz@users.noreply.github.com>
2023-10-13 12:24:27 +0200

    oleee

---
+++ libft/ft_strtrim.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bherranz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/27 13:07:23 by bherranz          #+#    #+#             */
+/*   Updated: 2023/09/27 13:07:29 by bherranz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*new;
+	int		start;
+	int		end;
+
+	start = 0;
+	if (!*s1)
+		end = 0;
+	else
+		end = ft_strlen(s1) - 1;
+	while (s1[start] && ft_strchr(set, s1[start]))
+		start++;
+	while (s1[end] && ft_strchr(set, s1[end]))
+		end--;
+	new = ft_substr(s1, start, end - start + 1);
+	return (new);
+}
+
+/*int	main(void)
+{
+	printf("%s", ft_strtrim("fa", ""));
+	return (0);
+}*/
